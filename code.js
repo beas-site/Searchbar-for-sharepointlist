e0ff6c68b155cd518d9b0d65ce18b3f804bdf2ae
beas-site <58391367+beas-site@users.noreply.github.com>
2020-09-27 10:18:46 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,9 @@
-
+<script type ="text/javascript">
+  
+  function Search() {
+  var searchText = document.getElementByID("Searchtxt").value;
+  var dropDown = document.getElementByID("coldropdwon").value;
+}
+  
+  
+  </script>
