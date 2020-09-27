de7d9b8bcbb37a9c54fae25c6193d0c0fde8772e
beas-site <58391367+beas-site@users.noreply.github.com>
2020-09-27 10:29:36 +0200

    Update code.js

--- code.js
+++ code.js
@@ -3,6 +3,17 @@
   function Search() {
   var searchText = document.getElementByID("Searchtxt").value;
   var dropDown = document.getElementByID("coldropdwon").value;
+  var url = "";
+  
+  if (searchText != "") {
+  if (dropDown = "" ||  dropDown = "" || dropDown = "" || dropDown = "" ||) {
+      url = "FilterField1" + dropDown + "&FilterValue1" + searchText;
+      window.location.href = "AllItems.aspx?" + url;
+      }
+      else {
+      url = "FilterName" + dropDown + "&FilterMultiValue=*" + searchText + "*";
+      }
+  }
 }
   
   
